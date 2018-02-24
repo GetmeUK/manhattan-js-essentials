b7b1784fa33b69a6eead9df49fd131c1dfa25814
Anthony Blackshaw <ant@getme.co.uk>
2018-02-24 00:32:52 +0000

    Updated dot location for eslint

--- .eslintrc.js
+++ .eslintrc.js
@@ -55,7 +55,7 @@ module.exports = {
         "consistent-this": "error",
         "curly": "error",
         "default-case": "error",
-        "dot-location": "error",
+        "dot-location": ["error", "property"],
         "dot-notation": "off",
         "eol-last": "error",
         "eqeqeq": "error",
