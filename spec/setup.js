6c5d08dbba47062c3473f746a16ef22ec00f2753
Anthony Blackshaw <ant@getme.co.uk>
2018-02-25 16:56:50 +0000

    Added window to globals in test setup

--- spec/setup.js
+++ spec/setup.js
@@ -1,6 +1,8 @@
 import * as jsdom from 'jsdom'
 
 const {JSDOM} = jsdom
+const {window} = (new JSDOM(''))
 const {document} = (new JSDOM('')).window
 
+global.window = window
 global.document = document
