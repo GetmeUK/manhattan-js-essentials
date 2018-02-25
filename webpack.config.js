c1760101fd409a4c8f7dc1507f6d575c27530d14
Anthony Blackshaw <ant@getme.co.uk>
2018-02-25 16:34:52 +0000

    Upgrade to webpack 4

--- webpack.config.js
+++ webpack.config.js
@@ -1,39 +1,31 @@
 var path = require('path');
-var webpack = require('webpack');
 
 
 module.exports = {
-     entry: {
-        'index': './module/index.js',
-        'index.min': './module/index.js'
-     },
+    entry: {
+        'index': './module/index.js'
+    },
 
-     output: {
-         library: "manhattan-essentials",
-         libraryTarget: "umd",
-         filename: "umd/[name].js"
-     },
+    output: {
+        library: 'manhattan-essentials',
+        libraryTarget: 'umd',
+        path: path.resolve(__dirname, 'umd'),
+        filename: '[name].js'
+    },
 
-     module: {
-         loaders: [
-             {
-                 test: /\.js$/,
-                 loader: 'babel-loader',
-                 query: {
-                     presets: ['es2015']
-                 }
-             }
-         ]
-     },
+    module: {
+        rules: [
+            {
+                test: /\.js$/,
+                loader: 'babel-loader',
+                query: {
+                    presets: ['es2015']
+                }
+            }
+        ]
+    },
 
-     plugins: [
-         new webpack.optimize.UglifyJsPlugin({
-             include: /\.min\.js$/,
-             minimize: true
-         })
-     ],
-
-     stats: {
+    stats: {
          colors: true
-     }
+    }
  };
